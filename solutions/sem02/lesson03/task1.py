7724efab7797a11c36d0d1134bde055606f9c8a6
f9ka9 <f9ka9@mail.ru>
2026-03-06 21:47:47 +0300

    Kamaleev Kemal B12-513 lesson3

--- solutions/sem02/lesson03/task1.py
+++ solutions/sem02/lesson03/task1.py
@@ -4,17 +4,25 @@ import numpy as np
 class ShapeMismatchError(Exception):
     pass
 
-
 def sum_arrays_vectorized(
     lhs: np.ndarray,
     rhs: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if lhs.shape!=rhs.shape:
+        raise ShapeMismatchError
+    return lhs+rhs
 
-
-def compute_poly_vectorized(abscissa: np.ndarray) -> np.ndarray: ...
-
+def compute_poly_vectorized(abscissa: np.ndarray) -> np.ndarray:
+    return 3*(abscissa**2)+2*abscissa+1
 
 def get_mutual_l2_distances_vectorized(
     lhs: np.ndarray,
     rhs: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if lhs.shape[1]!=rhs.shape[1]:
+        raise ShapeMismatchError
+    a2=np.sum(lhs**2, axis=1).reshape(-1,1)
+    b2 = np.sum(rhs**2, axis=1)
+    ab = np.dot(lhs, rhs.T)
+    return np.sqrt(a2+b2-2*ab)
+
