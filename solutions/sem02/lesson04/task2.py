2172871d1f1af71162354cce50b9c4a417dab00a
f9ka9 <f9ka9@mail.ru>
2026-03-13 23:50:29 +0300

    lesson4

--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -4,7 +4,7 @@ import numpy as np
 def get_dominant_color_info(img, t=5):
     if t < 1:
         raise ValueError("threshold must be positive")
-    p = img.reshape(-1)
+    p = img.flatten()
     c = [np.sum(p == i) for i in range(256)]
     m, b = 0, 0
     for i in range(256):
