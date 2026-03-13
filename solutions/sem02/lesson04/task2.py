7682791b162077dded5d2608e115bbf64d5ee427
f9ka9 <166027492+f9ka9@users.noreply.github.com>
2026-03-14 00:12:21 +0300

    Update task2.py

--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -1,15 +1,17 @@
 import numpy as np
 
-
-def get_dominant_color_info(img, t=5):
-    if t < 1:
+def get_dominant_color_info(
+    image: np.ndarray[np.uint8],
+    threshold: int = 5,
+) -> tuple[np.uint8, float]:
+    if threshold < 1:
         raise ValueError("threshold must be positive")
-    p = img.flatten()
+    p = image.flatten()
     c = [np.sum(p == i) for i in range(256)]
     m, b = 0, 0
     for i in range(256):
         if c[i]:
-            s = sum(c[max(0, i - t + 1) : min(256, i + t)])
+            s = sum(c[max(0, i - threshold + 1) : min(256, i + threshold)])
             if s > m:
                 m, b = s, i
     return np.uint8(b), float(m / len(p) * 100)
