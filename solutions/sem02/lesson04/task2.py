14be5d792aaa347d408309ffac4388455d9d52c5
f9ka9 <f9ka9@mail.ru>
2026-03-13 23:34:53 +0300

    lesson4

--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -1,10 +1,15 @@
 import numpy as np
 
 
-def get_dominant_color_info(
-    image: np.ndarray[np.uint8],
-    threshold: int = 5,
-) -> tuple[np.uint8, float]:
-    # ваш код
-
-    return 0, 0
+def get_dominant_color_info(img, t=5):
+    if t < 1:
+        raise ValueError("threshold must be positive")
+    p = img.ravel()
+    c = [np.sum(p == i) for i in range(256)]
+    m, b = 0, 0
+    for i in range(256):
+        if c[i]:
+            s = sum(c[max(0, i - t + 1) : min(256, i + t)])
+            if s > m:
+                m, b = s, i
+    return np.uint8(b), float(m / len(p) * 100)
