d7586abccc5f1818f69020137a1fc1d34b2244a7
kemal <f9ka9@mail.ru>
2026-04-10 23:50:01 +0300

    lesson7

--- solutions/sem01/lesson02/task3.py
+++ solutions/sem01/lesson02/task3.py
@@ -1,2 +1,3 @@
 def get_amount_of_ways_to_climb(stair_amount: int) -> int:
     # ваш код
+    pass
--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -1,7 +1,8 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
 from typing import Any
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 class ShapeMismatchError(Exception):
     pass
